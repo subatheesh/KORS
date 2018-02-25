30b37b238ed00fe1bb6cbaf7993374e916a29ae2
Subatheeesh R <subatheeshrt@gmail.ocm>
2018-02-25 20:48:48 +0530

    upto aboutus

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
+    $(document).scrollTop(0);
     var foregroundLeft = $("#foreGroundText").position().left;
     var backgroundLeft = $("#backGroundText").position().left;
+    var boldlinefirst = $("#boldLineFirst").position().left;
+    var boldlinesecond = $("#boldLineSecond").position().left;
+    var fixedparallaxsymbol = $("#fixedParallaxSymbol").position().left;
+    var fixedparallaxcircles = $("#fixedParallaxCircles").position().left;
     $(window).scroll(function(){
         var scrollTop = $(document).scrollTop();
-        $("#foreGroundText").css({left: foregroundLeft- 2*scrollTop});
-        $("#backGroundText").css({left: backgroundLeft- 2*scrollTop});
+        $("#foreGroundText").css({left: foregroundLeft - 2*scrollTop});
+        $("#backGroundText").css({left: backgroundLeft - 2*scrollTop});
+        $("#boldLineFirst").css({left: boldlinefirst - 2*scrollTop});
+        $("#boldLineSecond").css({left: boldlinesecond - 2*scrollTop});
+        $("#fixedParallaxSymbol").css({left: fixedparallaxsymbol - 1*scrollTop});
+        $("#fixedParallaxCircles").css({left: fixedparallaxcircles - 1*scrollTop});
     });
 });
