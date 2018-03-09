81931cce09f0100d50561fbc9e026b6593c59b00
Subatheeesh R <subatheeshrt@gmail.ocm>
2018-03-09 23:42:57 +0530

    gentlemen finished

--- gentle-men/js/main.js
+++ gentle-men/js/main.js
@@ -8,9 +8,23 @@ var adjustPage = function() {
     $("#BackgroundContainer").css("top", gentlemenImageHeight+150);
     $("#GentleMenDecoYellow").css("top", gentlemenImageHeight * 0.85);
     $("#GentleMenDecoWhite").css("top", gentlemenImageHeight * 0.75);
+    $("#GentleMenDecoBlueLeft").css("top",gentlemenImageHeight * 1.4);
+    if($(window).width() <= 420){
+      $("#ItemsContainer").css("padding-top", ItemsContainerTopPadding-70);
+      $("#BackgroundContainer").css("top", gentlemenImageHeight+30);
+      $("#BackgroundContainer").height(backgroundHeight+300);
+    }else if ($(window).width() <= 650) {
+      $("#ItemsContainer").css("padding-top", ItemsContainerTopPadding-70);
+      $("#BackgroundContainer").css("top", gentlemenImageHeight+30);
+      $("#BackgroundContainer").height(backgroundHeight+300);
+    }else if ($(window).width() <= 960) {
+      $("#ItemsContainer").css("padding-top", ItemsContainerTopPadding);
+      $("#BackgroundContainer").css("top", gentlemenImageHeight+ 100);
+      $("#BackgroundContainer").height(backgroundHeight + 150);
+    }
 };
 $(document).ready(function(){
-    setInterval(function() {
+    setTimeout(function() {
       adjustPage();
     },100);
     $(window).resize(function() {
