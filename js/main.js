524254dfe424e4a5b8ee4e8794cefa932da37242
Subatheeesh R <subatheeshrt@gmail.ocm>
2018-03-14 09:41:02 +0530

    some change in home page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,87 @@
+var mouseParallax = function(elem, x, y, etop, eleft, speed) {
+  // var y = event.clientY;
+  // var x = event.clientX;
+  var windowWidth = $(window).width();
+  var windowHeight = $(window).height();
+
+  var elemTop = etop + ((y - (windowHeight - (windowHeight / 2))) * speed / 100);
+  var elemLeft = eleft + ((x - (windowWidth - (windowWidth / 2))) * speed / 100);
+  elem.offset({top: elemTop, left: elemLeft});
+};
+
+var mouseParallaxInverse = function(elem, x, y, etop, eleft, speed) {
+  var x = event.clientX;
+  var y = event.clientY;
+  var windowWidth = $(window).width();
+  var windowHeight = $(window).height();
+
+  var elemTop = etop - ((y - (windowHeight - (windowHeight / 2))) * speed / 100);
+  var elemLeft = eleft - ((x - (windowWidth - (windowWidth / 2))) * speed / 100);
+  elem.offset({top: elemTop, left: elemLeft});
+};
+
+var mouseParallaxReverse = function(elem, x, y, etop, eleft, speed) {
+  var x = event.clientX;
+  var y = event.clientY;
+  var windowWidth = $(window).width();
+  var windowHeight = $(window).height();
+
+  var elemTop = etop + ((x - (windowWidth - (windowWidth / 2))) * speed / 100);
+  var elemLeft = eleft + ((y - (windowHeight - (windowHeight / 2))) * speed / 100);
+  elem.offset({top: elemTop, left: elemLeft});
+};
+
+var mouseParallaxReverseInverse = function(elem, x, y, etop, eleft, speed) {
+  var x = event.clientX;
+  var y = event.clientY;
+  var windowWidth = $(window).width();
+  var windowHeight = $(window).height();
+
+  var elemTop = etop - ((x - (windowWidth - (windowWidth / 2))) * speed / 100);
+  var elemLeft = eleft - ((y - (windowHeight - (windowHeight / 2))) * speed / 100);
+  elem.offset({top: elemTop, left: elemLeft});
+};
+
 $(document).ready(function(){
     $(document).scrollTop(0);
+
+    var skewcircleTop =  $("#SkewCircle").offset().top;
+    var skewcircleLeft =  $("#SkewCircle").offset().left;
+
+    var circlelineTop = $("#CircleLine").offset().top;
+    var circlelineLeft = $("#CircleLine").offset().left;
+
+    var circlewithoutcrossTop = $("#CircleWithoutCross").offset().top;
+    var circlewithoutcrossLeft = $("#CircleWithoutCross").offset().left;
+
+    var circlecrossTop = $("#CircleCross").offset().top;
+    var circlecrossLeft = $("#CircleCross").offset().left;
+
+    var cosmosilkytoptexttop = $("#CosmoSilkyTopText").offset().top;
+    var cosmosilkytoptextleft = $("#CosmoSilkyTopText").offset().left;
+
+    var cosmosilkybottomtexttop = $("#CosmoSilkyBottomText").offset().top;
+    var cosmosilkybottomtextleft = $("#CosmoSilkyBottomText").offset().left;
+
+    var haircarearrowstop = $("#HairCareArrows").offset().top;
+    var haircarearrowsleft = $("#HairCareArrows").offset().left;
+
+    var haircaredotstop = $("#HairCareDots").offset().top;
+    var haircaredotsleft = $("#HairCareDots").offset().left;
+
+    $(window).mousemove(function( event ) {
+        mouseParallax($("#SkewCircle"), event.clientX, event.clientY, skewcircleTop, skewcircleLeft, 2);
+        mouseParallaxInverse($("#CircleLine"), event.clientX, event.clientY, circlelineTop, circlelineLeft, 3);
+        mouseParallaxInverse($("#CircleWithoutCross"), event.clientX, event.clientY, circlewithoutcrossTop, circlewithoutcrossLeft, 4);
+        mouseParallax($("#CircleCross"), event.clientX, event.clientY, circlecrossTop, circlecrossLeft, 2);
+        mouseParallaxReverse($("#CosmoSilkyTopText"), event.clientX, event.clientY, cosmosilkytoptexttop, cosmosilkytoptextleft, 2);
+        mouseParallaxReverseInverse($("#CosmoSilkyBottomText"), event.clientX, event.clientY, cosmosilkybottomtexttop, cosmosilkybottomtextleft, 2);
+        mouseParallaxReverse($("#HairCareArrows"), event.clientX, event.clientY, haircarearrowstop, haircarearrowsleft, 1);
+        mouseParallax($("#HairCareDots"), event.clientX, event.clientY, haircaredotstop, haircaredotsleft, 3);
+    });
+
+
+
     var foregroundLeft = $("#foreGroundText").offset().left;
     var backgroundLeft = $("#backGroundText").offset().left;
     var boldlinefirst = $("#boldLineFirst").offset().left;
